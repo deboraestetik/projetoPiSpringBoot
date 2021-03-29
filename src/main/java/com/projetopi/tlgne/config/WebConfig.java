a6a546e19aee1356ce56805405551a41181863d4
Debora Souza <debora.r.souza@globalhitss.com.br>
2021-03-29 13:55:51 -0300

    Configurando cors corrigindo

--- src/main/java/com/projetopi/tlgne/config/WebConfig.java
+++ src/main/java/com/projetopi/tlgne/config/WebConfig.java
@@ -7,31 +7,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
-        List<String> all = Arrays.asList("*");
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
+//        List<String> all = Arrays.asList("*");
+//
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedOrigins(all);
+//        corsConfiguration.setAllowedHeaders(all);
+//        corsConfiguration.setAllowedMethods(all);
+//        corsConfiguration.setAllowCredentials(true);
+//
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfiguration);
+//
+//        CorsFilter corsFilter = new CorsFilter(source);
+//        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
+//        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return filter;
+//    }
 
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(all);
-        corsConfiguration.setAllowedHeaders(all);
-        corsConfiguration.setAllowedMethods(all);
-        corsConfiguration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-
-        CorsFilter corsFilter = new CorsFilter(source);
-        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
-        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return filter;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
+
+
